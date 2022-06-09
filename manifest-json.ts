71272a7b0ed486e595df7ef879701e56c9c17c66
Jack Steam <jacksteamdev@gmail.com>
2022-06-09 16:24:17 -0500

    fix: remove unnecessary manifest action key

--- manifest-json.ts
+++ manifest-json.ts
@@ -4,7 +4,6 @@ export const manifest = defineManifest({
   manifest_version: 3,
   name: 'Jack Steam Take Home Tab Search',
   version: '0.0.1',
-  action: {},
   background: {
     service_worker: 'src/background.ts',
     type: 'module',
